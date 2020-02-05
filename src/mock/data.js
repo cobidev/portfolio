f6b4580059aa114f5acd6ccc4e6b4a31b7b3a763
Jacobo Martinez <jacobo.martinez@mediamonks.com>
2020-02-05 15:36:20 -0300

    update website to gatsby

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,25 +2,28 @@ import uuidv1 from 'uuid/v1';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Jacobo Martinez | Software Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Jacobo Martinez official website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi, my name is',
+  name: 'Jacobo',
+  subtitle: 'I like to make stuff with code',
+  cta: 'Know more',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  img: 'profile.JPG',
+  paragraphOne:
+    "I'm a professional and passionate Software Developer focused on efficiency and constant learning.",
+  paragraphTwo:
+    'I have created solutions for my clients by improving their web development processes and accomplish their ideas come true, either from the front-end or the back-end mostly using JavaScript, React, Gatsby, Node, Express.js & MongoDB.',
+  paragraphThree:
+    'I would love to set up an introduction call to discuss how I could help your business and make your goals happen.',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -28,38 +31,81 @@ export const aboutData = {
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'simplefolio.png',
+    title: 'Gatsby Simplefolio',
+    info: 'A minimal portfolio template for Developers',
+    info2:
+      'This project was highlighted in one of the highest valorated courses on Udemy, called Zero To Mastery.',
+    url: 'https://simplfolio.netlify.com',
+    repo: 'https://github.com/cobidev/simplefolio', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'mososla.png',
+    title: "Mososla's Agency Website",
+    info: 'A Gatsby website for a Digital Agency based in Panama.',
+    info2: 'I used Gatsby & GraphQL + Shopify API in order to create this website.',
+    url: 'https://mososla.network',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'mockraft.png',
+    title: "Mockraft's Website",
+    info: 'A React website for a Digital Agency based in Caracas.',
+    info2: 'I used React, React-Router & React Context API in order to create this website.',
+    url: 'https://mockraft.com',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'mura.jpg',
+    title: 'Perfil de Escucha',
+    info:
+      'A MVC Web Application that allows you to identify your predominant profile in listening and behavior areas to suggest solutions in your communicational field.',
+    info2:
+      'I developed this project for Murá, a recognized consultant from the Caracas, Venezuela.',
+    url: 'https://mura-listening-profile.herokuapp.com/',
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'face-app.jpg',
+    title: 'Face Recognition App',
+    info:
+      "A full-stack web application using the (MERN) Stack, this app allows you to identify the detection of faces in any image supplied. This was achieved thanks to Clarifai's artificial intelligence API.",
+    info2: 'I used React, Node, Express.js and MongoDB in order to create this web application.',
+    url: 'https://face-recognition-cobimr.herokuapp.com/',
+    repo: 'https://github.com/cobidev/face-recognition', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'tw-clone.jpg',
+    title: 'Twitter Clone',
+    info:
+      'A simple and minimalistic clone of Twitter, this full-stack application allows you to post tweets, see user profiles, follow or unfollow, and do the basic CRUD operations.',
+    info2:
+      'I used Node/Express to create the REST API, Passport.js for user authentication, Pug for template engine and MongoDB for data storage.',
+    url: 'https://face-recognition-cobimr.herokuapp.com/',
+    repo: 'https://github.com/cobidev/face-recognition', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: 'rpc.png',
+    title: 'Rock, Papper or Scissor',
+    info: 'My first Front-End game from scratch using Vanilla Javascript, CSS3 and HTML5.',
+    info2:
+      'Additionally, thinking on refactor this application very soon with React & React-Router',
+    url: 'https://rockpaperscissors-game.netlify.com/',
+    repo: 'https://github.com/cobidev/rock-paper-scissors', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: '"Would you like to work with me? Awesome!',
+  btn: "Let's Talk",
+  email: 'cobimr@protonmail.com',
 };
 
 // FOOTER DATA
@@ -68,22 +114,22 @@ export const footerData = {
     {
       id: uuidv1(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/cobidec',
     },
     {
       id: uuidv1(),
       name: 'codepen',
-      url: '',
+      url: 'https://codepen.io/cobimr',
     },
     {
       id: uuidv1(),
       name: 'linkedin',
-      url: '',
+      url: 'https://likedin.com/in/cobidev',
     },
     {
       id: uuidv1(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/cobidev',
     },
   ],
 };
