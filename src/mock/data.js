5235b52e537cb9911c2d67a5654b6817dabdd628
Jacobo Martinez <jacobo.martinez@mediamonks.com>
2020-03-01 01:00:56 -0300

    update again data lol

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,7 +19,7 @@ export const heroData = {
 export const aboutData = {
   img: 'profile.jpg',
   paragraphOne:
-    'I am a Web Developer with enthusiasm and focus on frontend development and scripting.',
+    'I am a Web Developer with enthusiasm and focus on Frontend Development and Scripting.',
   paragraphTwo:
     'I am passionate about building scalable software, creating effective solutions, and learning every day to grow professionally in the IT field.',
   paragraphThree: 'Feel free to contact me via email at any time',
